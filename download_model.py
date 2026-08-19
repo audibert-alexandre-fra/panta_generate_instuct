326216e43777768540c643f0bf3859c37fb1e7ab
Alexandre Audibert <audibeal@racer.imag.fr>
2026-08-19 14:48:23 +0200

    download_model.py

--- download_model.py
+++ download_model.py
@@ -13,7 +13,7 @@ import argparse
 
 from huggingface_hub import snapshot_download
 
-DEFAULT_MODEL = "paraphrase-multilingual-MiniLM-L12-v2"
+DEFAULT_MODEL = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 
 
 def main() -> None:
